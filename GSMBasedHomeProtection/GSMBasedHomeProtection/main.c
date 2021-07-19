8b33604fae2e910cd1d84379bb62878373a99817
Sadat Shahriyar <shahriyarsadat@gmail.com>
2021-07-19 08:52:36 +0600

    Number updated

--- GSMBasedHomeProtection/GSMBasedHomeProtection/main.c
+++ GSMBasedHomeProtection/GSMBasedHomeProtection/main.c
@@ -7,11 +7,15 @@
 #define EN eS_PORTC7
 
 
-//#define DEFAULT_RESPONSE_SIZE 2000
+//#define SREG    _SFR_IO8(0x3F)
+//
+//
+//#define DEFAULT_RESPONSE_SIZE 1200
 
 #include <avr/io.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <avr/sleep.h>
 //#include <bits/stdc++.h>
 #include "lcd.h"
 #include "adc.h"
@@ -21,11 +25,11 @@
 
 
 char ATcommand[] = {'A', 'T', '\n\r', '\0'};
-char ATDcommand[] = {'A', 'T', 'D', '+', '8', '8', '0', '1', '7','9','9', '3', '5', '8', '5', '9', '1', ';', '\n\r', '\0'};
+char ATDcommand[] = {'A', 'T', 'D', '+', '8', '8', '0', '1', '7','8','9', '9', '9', '1', '6', '6', '7', ';', '\n\r', '\0'};
 char ATHcommand[] = {'A', 'T','H', '\n\r', '\0'};
 char setBaudRate[] = {'A', 'T', '+', 'I', 'P', 'R', '=', '9', '6', '0', '0', '\n\r','\0'};
 char ATCMGFcommand[] = {'A', 'T', '+', 'C', 'M', 'G', 'F', '=', '1','\n\r','\0'};
-char ATCMGScommand[] = {'A', 'T', '+', 'C', 'M', 'G', 'S', '=', '"', '+', '8', '8', '0', '1', '7','9','9', '3', '5', '8', '5', '9', '1', '"', '\n\r', '\0'};
+char ATCMGScommand[] = {'A', 'T', '+', 'C', 'M', 'G', 'S', '=', '"', '+', '8', '8', '0', '1', '7','8','9', '9', '9', '1', '6', '6', '7', '"', '\n\r', '\0'};
 char ATSapbrPost1[] = {'A','T', '+', 'S', 'A', 'P', 'B', 'R', '=', '3', ',' , '1',',', '"', 'C', 'o', 'n', 't', 'y', 'p', 'e', '"', ',', '"', 'G', 'P', 'R', 'S', '"', '\n\r' , '\0'};
 char ATSapbrPost2[] = {'A','T', '+', 'S', 'A', 'P', 'B', 'R', '=', '3', ',' , '1', '"', 'A', 'P', 'N', '"', ',', '"','R', 'o', 'b', 'i', '-', 'I', 'N', 'T', 'E', 'R', 'N', 'E', 'T', '"', '\n\r' , '\0'};
 char ATSapbrPost3[] = {'A','T', '+', 'S', 'A', 'P', 'B', 'R', '=', '1', ',' , '1', '\n\r' , '\0'};
@@ -35,15 +39,15 @@ char AThttpparaPost6[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A',
 char AThttpparaPost7[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e', '"', '\n\r', '\0'};
 char ATHTTPDataPost8[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'D', 'A', 'T', 'A', '=', '0',',', '1', '0','0','0','0', '\n\r', '\0'};
 char writeApikeyPost9[] = {'a', 'p', 'i', '_', 'k', 'e', 'y', '=', 'K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O', '&','f','i','e','l','d','1','=','1','\n\r', '\0'};
-char ATHTTPAction1Post10[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'A', 'C', 'T', 'I', 'O', 'N', '=', '1', '\n\r', '\0'}; 
+char ATHTTPAction1Post10[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'A', 'C', 'T', 'I', 'O', 'N', '=', '1', '\n\r', '\0'};
 
 
 char ATURL[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e',  '?','a','p','i','_','k','e','y','=','K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O','&','f','i','e','l','d','1','=','1','0', '0', '"', '\n\r', '\0'};
-char ATHTTPAction0[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'A', 'C', 'T', 'I', 'O', 'N', '=', '0', '\n\r', '\0'}; 
+char ATHTTPAction0[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'A', 'C', 'T', 'I', 'O', 'N', '=', '0', '\n\r', '\0'};
 char ATHTTPRead[] = {'A','T','+','H','T','T','P','R','E','A','D', '\n\r', '\0'};
 char ATHTTPTerm[] = {'A','T','+','H','T','T','P','T','E','R','M','\n\r', '\0'};
-	
-//char response[DEFAULT_RESPONSE_SIZE];
+
+//unsigned char response[2000];
 //uint16_t counter = 0;
 
 unsigned char Res[16], lpg[16], smoke[16];
@@ -52,6 +56,8 @@ volatile uint8_t gc,fc;
 volatile unsigned char toggle = 0;
 volatile uint16_t pirTimeCount = 0;
 volatile float flame = 0, smoke2 = 0, lpg2 = 0;
+//unsigned char response[500];
+//int counter = 0;
 
 ISR(INT1_vect)//STEP2
 {
@@ -66,39 +72,56 @@ ISR(INT1_vect)//STEP2
 
 ISR(TIMER1_OVF_vect){
 	sei();
+	if(toggle == 1)
 	pirTimeCount++;
+	else pirTimeCount = 0;
 }
-
-ISR(INT0_vect){
-	if(toggle == 1){
-		sei();
-		pirTimeCount = 0;
-		Lcd4_Clear();
-		Lcd4_Set_Cursor(2,1);
-		Lcd4_Write_String("Motion detected");
-		_delay_ms(2000);
-		motionDetected();
-	}
-}
+//
+//ISR(INT0_vect){
+//if(toggle == 1){
+//sei();
+//pirTimeCount = 0;
+//Lcd4_Clear();
+//Lcd4_Set_Cursor(2,1);
+//Lcd4_Write_String("Motion detected");
+//_delay_ms(2000);
+//motionDetected();
+//}
+////Lcd4_Clear();
+////Lcd4_Set_Cursor(1,1);
+////Lcd4_Write_String("Int");
+////_delay_ms(1500);
+//}
 
 //ISR(USART_RXC_vect){
-	//sei();
-	//response[counter++] = UDR;
+//sei();
+//response[counter++] = UDR;
 //}
 //
 //void responseBufferFlush(){
-	//memset(response, 0, DEFAULT_RESPONSE_SIZE);
-	//counter = 0;
+//memset(response, 0, DEFAULT_RESPONSE_SIZE);
+//counter = 0;
 //}
+
+//ISR (USART_RXC_vect) {	/* Receive ISR routine */
+////uint8_t oldsrg = SREG;
+//sei();
+//response[counter++] = UDR;
+////SREG = oldsrg;
+//}
+//
 
 void USART_init(uint16_t ubrr_value)
 {
-	UCSRA = 0b00000000;  
+	UCSRA = 0b00000000;
 	UCSRB = 0b00011000;  // Enable Tx and Rx, polling
 	UCSRC = 0b10000110;  // Async mode, no parity, 1 stop bit, 8 data bits
+	//SREG = 0x3F;
 	//in double-speed mode, UBRR = clock/(8xbaud rate) - 1
 	UBRRH = 0;
 	UBRRL = 51; // Baud rate 1200bps, assuming 1MHz clock
+	//set_sleep_mode(SLEEP_MODE_IDLE);
+	//sei();
 }
 
 void USART_send(unsigned char data){
@@ -123,7 +146,7 @@ void USART_send_string(char c[]){
 
 void GSM_init(){
 	Lcd4_Clear();
-	Lcd4_Write_String("Initializing GSM");	
+	Lcd4_Write_String("Initializing GSM");
 	_delay_ms(7000);
 	USART_send_string(ATcommand);
 	//USART_send_string(setBaudRate);
@@ -134,6 +157,8 @@ void GSM_init(){
 }
 
 void GSMcall(){
+	_delay_ms(2000);
+	PORTB = 0x00;
 	Lcd4_Clear();
 	Lcd4_Write_String("Calling");
 	//_delay_ms(20000);
@@ -198,12 +223,66 @@ float takeGasSensorInput(){
 	return result;
 }
 
+
+void HTTPSendTest2(float lpgVal, float smokeVal, float temperatureVal, float motionDetectionTime, float PIRStatus){
+	//responseBufferFlush();
+	
+	Lcd4_Clear();
+	Lcd4_Set_Cursor(1,1);
+	Lcd4_Write_String("Sending data");
+	Lcd4_Set_Cursor(2,1);
+	Lcd4_Write_String("to server");
+	//char ATURL[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e',  '?','a','p','i','_','k','e','y','=','K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O','&','f','i','e','l','d','1','=','1','0', '0', '"', '\n\r', '\0'};
+
+	char command[100];
+	sprintf(command, "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/update?api_key=KRJ5C7MAMRWHSSKO&field1=%d&field2=%d&field3=%d&field4=%d&field5=%d\"", (int)lpgVal, (int)smokeVal, (int)temperatureVal, (int)motionDetectionTime, (int)PIRStatus);
+	
+	USART_send_string(ATcommand);
+	_delay_ms(1500);
+	USART_send_string(ATSapbrPost1);
+	_delay_ms(1500);
+	USART_send_string(ATSapbrPost2);
+	_delay_ms(1500);
+	USART_send_string(ATSapbrPost3);
+	_delay_ms(1500);
+	USART_send_string(ATSapbrPost4);
+	_delay_ms(1500);
+	USART_send_string(httpiInitPost5);
+	_delay_ms(1500);
+	USART_send_string(AThttpparaPost6);
+	_delay_ms(1500);
+	//USART_send_string(ATURL);
+	USART_send_string(command);
+	USART_send('\n\r');
+	_delay_ms(1500);
+	USART_send_string(ATHTTPAction0);
+	_delay_ms(1500);
+	USART_send_string(ATHTTPRead);
+	_delay_ms(1500);
+	USART_send_string(ATHTTPTerm);
+	_delay_ms(1500);
+	
+	Lcd4_Clear();
+	Lcd4_Set_Cursor(1,1);
+	Lcd4_Write_String("sent");
+	_delay_ms(15000);
+}
+
+int getPirTime(){
+	int timeCount = (pirTimeCount*65536+TCNT1)/(60*1000000);
+	if(toggle == 1)
+	return timeCount;
+	else return 0;
+}
+
+
 void gasDetected(){
 	ledLightAndBuzzer();
 	GSMcall();
 	_delay_ms(5000);
-	char mes[] = "Critical level gas detected";	
+	char mes[] = "Critical level gas detected";
 	GSMSendMessage(mes);
+	HTTPSendTest2(lpg2, smoke2, flame, getPirTime(), toggle);
 }
 
 void flameDetected(){
@@ -212,6 +291,7 @@ void flameDetected(){
 	_delay_ms(5000);
 	char mes[] = "Fire detected";
 	GSMSendMessage(mes);
+	HTTPSendTest2(lpg2, smoke2, flame, getPirTime(), toggle);
 }
 void motionDetected(){
 	ledLightAndBuzzer();
@@ -219,6 +299,7 @@ void motionDetected(){
 	_delay_ms(5000);
 	char mes[] = "motion detected";
 	GSMSendMessage(mes);
+	HTTPSendTest2(lpg2, smoke2, flame, getPirTime(), toggle);
 }
 
 void gasSensorInit(){
@@ -255,7 +336,7 @@ void gasSensorInit(){
 
 
 void getGasSensorInput(){
-	//Lcd4_Clear();
+	Lcd4_Clear();
 	Lcd4_Set_Cursor(1,1);
 	Lcd4_Write_String("LPG:");
 	Lcd4_Set_Cursor(1,13);
@@ -266,12 +347,12 @@ void getGasSensorInput(){
 	Lcd4_Write_String("PPM");
 	
 	float lpgAmount = GetGasPercentage(ReadSensor()/Ro, LPG);
-	itoa(lpgAmount, lpg,10);	
+	itoa(lpgAmount, lpg,10);
 	Lcd4_Set_Cursor(1,5);
 	Lcd4_Write_String(lpg);
 	
 	
-	if(lpgAmount < 0) lpgAmount = 99999;
+	if(lpgAmount < 0) lpgAmount = 65535;
 	lpg2 = lpgAmount;
 	
 	int smokeAmount = GetGasPercentage(ReadSensor()/Ro,SMOKE);
@@ -279,7 +360,7 @@ void getGasSensorInput(){
 	Lcd4_Set_Cursor(2,7);
 	Lcd4_Write_String(smoke);
 	smoke2 = (float)smokeAmount;
-	
+	_delay_ms(1500);
 	if(lpgAmount > 50){
 		Lcd4_Clear();
 		Lcd4_Set_Cursor(1,1);
@@ -302,6 +383,7 @@ void getGasSensorInput(){
 	Lcd4_Set_Cursor(2,7);
 	Lcd4_Write_String("      ");
 }
+
 float flameSensorInit(){
 	float val=0;
 	for(int i=0;i<10;i++){
@@ -317,14 +399,14 @@ float getTemperatureInput(){
 	return temperature;
 }
 
-void flameSensorInput(){	
+void flameSensorInput(){
 	
 	int flameAnalogValue=adcread(1);
 	char fval[5];
 	dtostrf(flameAnalogValue,3,1,fval);
 	Lcd4_Clear();
 	Lcd4_Set_Cursor(1,1);
-	Lcd4_Write_String("flame: ");	
+	Lcd4_Write_String("flame: ");
 	Lcd4_Write_String(fval);
 	Lcd4_Write_String(" nm");
 	_delay_ms(500);
@@ -347,57 +429,53 @@ void flameSensorInput(){
 		//Lcd4_Write_String("fc: ");
 		//Lcd4_Write_String(fval);
 		//if(fc==3){
-			Lcd4_Clear();
-			Lcd4_Set_Cursor(1,1);
-			Lcd4_Write_String("flame detected");
-			_delay_ms(3000);
-			flameDetected();
-			
+		Lcd4_Clear();
+		Lcd4_Set_Cursor(1,1);
+		Lcd4_Write_String("flame detected");
+		_delay_ms(3000);
+		flameDetected();
+		
 		//}
 		
 	}
 	//else{
-		////fc=0;
+	////fc=0;
 	//}
 }
 void motionSensorInput(){
 	//int motionAnalogValue=adcread(2);
 	char fval[5];
 	//dtostrf(motionAnalogValue,3,1,fval);
+	
+	int pir=adcread(3);
+	
+	_delay_ms(1000);
+	
 	if(toggle == 1){
+		
 		Lcd4_Clear();
 		Lcd4_Set_Cursor(1,1);
 		Lcd4_Write_String("PIR active");
-		_delay_ms(1000);
+		if(pir > 600){
+			Lcd4_Set_Cursor(2,1);
+			Lcd4_Write_String("Motion Detected");
+			_delay_ms(2000);
+			motionDetected();
+		}
+		else{
+			Lcd4_Set_Cursor(2,1);
+			Lcd4_Write_String("No Motion");
+			_delay_ms(2000);
+		}
 	}
 	else{
 		Lcd4_Clear();
 		Lcd4_Set_Cursor(1,1);
 		Lcd4_Write_String("PIR inactive");
-		_delay_ms(1000);
-	}	
-	//unsigned char in = PINB & 0x01;
-	//if(in && toggle){
-		//Lcd4_Clear();
-		//Lcd4_Set_Cursor(1,1);
-		//Lcd4_Write_String("Motion detected");
-		//_delay_ms(2000);
-		//motionDetected();
-		//
-	//}
-	//else{
-		//Lcd4_Clear();
 		Lcd4_Set_Cursor(2,1);
 		Lcd4_Write_String("No Motion");
 		_delay_ms(2000);
-	//}
-	//dtostrf(in, 2,1,fval);
-	//Lcd4_Clear();
-	//Lcd4_Set_Cursor(1,1);
-	//Lcd4_Write_String("motion: ");
-	//Lcd4_Write_String(fval);
-	//Lcd4_Write_String(" ");
-	//_delay_ms(1000);
+	}
 }
 
 void ledLightAndBuzzer(){
@@ -435,96 +513,57 @@ void HTTPSendTest(){
 	Lcd4_Clear();
 	Lcd4_Write_String("sent");
 }
-
-void HTTPSendTest2(float lpgVal, float smokeVal, float temperatureVal, float motionDetectionTime, float PIRStatus){
-	//responseBufferFlush();
-	
-	Lcd4_Clear();
-	Lcd4_Set_Cursor(1,1);
-	Lcd4_Write_String("Sending");
-	//char ATURL[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e',  '?','a','p','i','_','k','e','y','=','K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O','&','f','i','e','l','d','1','=','1','0', '0', '"', '\n\r', '\0'};
-
-	char command[100];
-	sprintf(command, "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/update?api_key=KRJ5C7MAMRWHSSKO&field1=%d&field2=%d&field3=%d&field4=%d&field5=%d\"", (int)lpgVal, (int)smokeVal, (int)temperatureVal, (int)motionDetectionTime, (int)PIRStatus);
-	
-	USART_send_string(ATcommand);
-	_delay_ms(1500);
-	USART_send_string(ATSapbrPost1);
-	_delay_ms(1500);
-	USART_send_string(ATSapbrPost2);
-	_delay_ms(1500);
-	USART_send_string(ATSapbrPost3);
-	_delay_ms(1500);
-	USART_send_string(ATSapbrPost4);
-	_delay_ms(1500);
-	USART_send_string(httpiInitPost5);
-	_delay_ms(1500);
-	USART_send_string(AThttpparaPost6);
-	_delay_ms(1500);
-	//USART_send_string(ATURL);
-	USART_send_string(command);
-	USART_send('\n\r');
-	_delay_ms(1500);
-	USART_send_string(ATHTTPAction0);
-	_delay_ms(1500);
-	USART_send_string(ATHTTPRead);
-	_delay_ms(1500);
-	USART_send_string(ATHTTPTerm);
-	_delay_ms(1500);
-	
-	Lcd4_Clear();
-	Lcd4_Set_Cursor(1,1);
-	Lcd4_Write_String("sent");
-	_delay_ms(15000);
-}
-
-
-//void HTTPGetTest(){
-	//responseBufferFlush();
-	//
-	//Lcd4_Clear();
-	//Lcd4_Write_String("Receiving");
-	////char ATURL[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e',  '?','a','p','i','_','k','e','y','=','K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O','&','f','i','e','l','d','1','=','1','0', '0', '"', '\n\r', '\0'};
 //
-	//char command[100];
-	//sprintf(command, "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/channels/1446074/fields/5.json?results=1\"");
-	//
-	//USART_send_string(ATcommand);
-	//_delay_ms(1500);
-	//USART_send_string(ATSapbrPost1);
-	//_delay_ms(1500);
-	//USART_send_string(ATSapbrPost2);
-	//_delay_ms(1500);
-	//USART_send_string(ATSapbrPost3);
-	//_delay_ms(1500);
-	//USART_send_string(ATSapbrPost4);
-	//_delay_ms(1500);
-	//USART_send_string(httpiInitPost5);
-	//_delay_ms(1500);
-	//USART_send_string(AThttpparaPost6);
-	//_delay_ms(1500);
-	////USART_send_string(ATURL);
-	//USART_send_string(command);
-	//USART_send('\n\r');
-	//_delay_ms(1500);
-	//USART_send_string(ATHTTPAction0);
-	//_delay_ms(1500);
-	//USART_send_string(ATHTTPRead);
-	//_delay_ms(1500);
-	//USART_send_string(ATHTTPTerm);
-	//_delay_ms(1500);
-	//
-	//Lcd4_Clear();
-	//Lcd4_Write_String("sent");
-	//_delay_ms(2000);
-	//
-	////Lcd4_Clear();
-	////Lcd4_Write_String(response);
-	////_delay_ms(500);
-	////for(int i=0;i<100;i++){
-		//////Lcd4_Shift_Left();
-		////_delay_ms(500);
-	////}
+//void HTTPGetTest(){
+//responseBufferFlush();
+//
+//Lcd4_Clear();
+//Lcd4_Write_String("Receiving");
+////char ATURL[] = {'A', 'T', '+', 'H', 'T', 'T', 'P', 'P', 'A', 'R', 'A', '=', '"', 'U', 'R', 'L','"', ',', '"', 'a', 'p', 'i', '.', 't', 'h', 'i', 'n', 'g', 's', 'p', 'e', 'a', 'k', '.', 'c', 'o', 'm', '/', 'u', 'p', 'd', 'a', 't', 'e',  '?','a','p','i','_','k','e','y','=','K','R','J','5','C','7','M','A','M','R','W','H','S','S','K','O','&','f','i','e','l','d','1','=','1','0', '0', '"', '\n\r', '\0'};
+//
+//char command[100];
+//sprintf(command, "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/channels/1446074/fields/5.json?results=1\"");
+//
+//USART_send_string(ATcommand);
+//_delay_ms(1500);
+//USART_send_string(ATSapbrPost1);
+//_delay_ms(1500);
+//USART_send_string(ATSapbrPost2);
+//_delay_ms(1500);
+//USART_send_string(ATSapbrPost3);
+//_delay_ms(1500);
+//USART_send_string(ATSapbrPost4);
+//_delay_ms(1500);
+//USART_send_string(httpiInitPost5);
+//_delay_ms(1500);
+//USART_send_string(AThttpparaPost6);
+//_delay_ms(1500);
+////USART_send_string(ATURL);
+//USART_send_string(command);
+//USART_send('\n\r');
+//_delay_ms(1500);
+//USART_send_string(ATHTTPAction0);
+//_delay_ms(1500);
+//USART_send_string(ATHTTPRead);
+//_delay_ms(1500);
+//USART_send_string(ATHTTPTerm);
+//_delay_ms(1500);
+//
+//Lcd4_Clear();
+//Lcd4_Write_String("sent");
+//_delay_ms(2000);
+//
+//Lcd4_Clear();
+//Lcd4_Set_Cursor(1,1);
+//Lcd4_Write_String("read");
+//
+//Lcd4_Clear();
+//Lcd4_Write_String(response);
+//_delay_ms(500);
+//for(int i=0;i<100;i++){
+//Lcd4_Shift_Left();
+//_delay_ms(500);
+//}
 //}
 
 
@@ -535,32 +574,28 @@ void timerInit(){
 	TIMSK = 1 << TOIE1;
 }
 
-int getPirTime(){
-	int timeCount = (pirTimeCount*65535+TCNT1)/60;
-	return timeCount;
-}
 
 int main(void)
-{	
+{
 	gc = 0;
 	fc = 0;
-    DDRC = 0xFF;
-    DDRD = 0xFF;
+	DDRC = 0xFF;
+	DDRD = 0xFF;
 	DDRA = 0x00;
 	DDRB= 0x06;
 	ADMUX = 0b01000000;
 	ADCSRA = 0b10000111;
 	
 	PORTB = 0x00;
-	MCUCR = 1 << ISC10 | 1 << ISC11 | 1 << ISC00 | 1 << ISC01 ;
-	GICR = (1<<INT1 | 1 << INT0); //STEP3
+	MCUCR = 1 << ISC10 | 1 << ISC11  ;
+	GICR = (1<<INT1); //STEP3
 	//MCUCR = MCUCR & 0b11110011;//STEP4
 	sei();//STEP5
 	
 	timerInit();
 
 	USART_init(51);
-    Lcd4_Init();
+	Lcd4_Init();
 	
 	_delay_ms(2000);
 	
@@ -573,18 +608,23 @@ int main(void)
 	//_delay_ms(2000);
 	
 	//HTTPSendTest();
-	uint16_t val = 0;
-    while (1) 
-    {
+	uint16_t val = 1;
+	while (1)
+	{
 		getGasSensorInput();
 		PORTB = 0x00;
 		flameSensorInput();
 		PORTB = 0x00;
 		motionSensorInput();
 		PORTB = 0x00;
-		HTTPSendTest2(lpg2, smoke2, flame, getPirTime(), toggle);
-		//val++;
-    }
+		if(val%5 == 0){
+			HTTPSendTest2(lpg2, smoke2, flame, getPirTime(), toggle);
+			val = 0;
+		}
+		val++;
+		//HTTPGetTest();
+		_delay_ms(1000);
+	}
 	
 	return 0;
 }
